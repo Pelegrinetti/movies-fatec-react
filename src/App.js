f0bea1ade606d6f2227b2bd719a2c2da397029bf
Pelegrinetti <netispanda@gmail.com>
2019-06-08 14:03:13 -0300

    feat: Adicionado loader e busca

--- src/App.js
+++ src/App.js
@@ -1,56 +1,67 @@
-import React, { Component } from 'react';
-import { PacmanLoader } from 'react-spinners'
+import React, { Component } from "react";
+import { PacmanLoader } from "react-spinners";
 
-import Movie from './components/Movie'
+import Movie from "./components/Movie";
 
 export default class App extends Component {
-
   state = {
     movies: [],
-    query: '',
+    query: "",
     loading: false
-  }
+  };
 
   handleSetQuery = e => {
-    const query = e.target.value
-    console.log(query)
-    this.setState({ query })
-  }
+    const query = e.target.value;
 
-  handleSubmit = async () => {
-    this.setState({ movies: [], loading: true })
+    this.setState({ query });
+  };
 
-    const response = await fetch(`https://www.omdbapi.com/?s=${this.state.query}&apikey=1c889ae`)
-    const data = await response.json()
-    this.setState({ movies: data.Response ? data.Search : [], loading: false })
-  }
+  handleSubmit = async e => {
+    e.preventDefault();
+    this.setState({ movies: [], loading: true });
 
-  render(){
+    const response = await fetch(
+      `https://www.omdbapi.com/?s=${this.state.query}&apikey=1c889ae`
+    );
+    const data = await response.json();
+    this.setState({ movies: data.Response ? data.Search : [], loading: false });
+  };
+
+  render() {
     return (
       <div className="container">
-        <div className="search">
-          <input className="input" placeholder="Procurar filme..." type="text" onChange={this.handleSetQuery} />
-          <button className="btn" onClick={this.handleSubmit}>Go</button>
-        </div>
+        <form className="search" onSubmit={this.handleSubmit}>
+          <input
+            className="input"
+            placeholder="Procurar filme..."
+            type="text"
+            onChange={this.handleSetQuery}
+          />
+          <button type="submit" className="btn">
+            Go
+          </button>
+        </form>
         <section className="movies">
           <div className="results">
-          { this.state.movies ? (
-            <>
-              <PacmanLoader
-                sizeUnit={"px"}
-                size={50}
-                color={'#fff'}
-                loading={this.state.loading}
-              />
-              { this.state.movies.map(movie => (
-                <Movie key={movie.imdbID} cover={movie.Poster} />
-              )) }
-            </>
-          ) : <p>Não há filmes aqui...</p> }
+            {this.state.movies ? (
+              <>
+                <PacmanLoader
+                  sizeUnit={"px"}
+                  size={50}
+                  color={"#fff"}
+                  loading={this.state.loading}
+                />
+                {this.state.movies.map(movie => (
+                  <Movie key={movie.imdbID} cover={movie.Poster} />
+                ))}
+              </>
+            ) : (
+              <p>Não há filmes aqui...</p>
+            )}
           </div>
-          <div className="favorites"></div>
+          <div className="favorites" />
         </section>
       </div>
-    )
+    );
   }
 }
